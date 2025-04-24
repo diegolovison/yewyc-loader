ccc8c5828e3be16913b71023f96a5e599fe6a51c
Diego Lovison <diegolovison@gmail.com>
2025-04-24 09:00:24 -0300

    Increase the test to 60s

--- java-http-client/src/main/java/com/github/yewyc/javahttpclient/JavaHttpClientSimplePerfTest.java
+++ java-http-client/src/main/java/com/github/yewyc/javahttpclient/JavaHttpClientSimplePerfTest.java
@@ -6,7 +6,7 @@ import com.github.yewyc.MeasureLatencyType;
 public class JavaHttpClientSimplePerfTest extends JavaHttpClientTask {
 
     public static void main(String[] args) {
-        try (MeasureLatency measure = new MeasureLatency(10, 100, 1, 5, MeasureLatencyType.GLOBAL)
+        try (MeasureLatency measure = new MeasureLatency(60, 100, 1, 60, MeasureLatencyType.GLOBAL)
                 .addTask(createTask1()).start()) {
             measure.generateReport().plot();
         }
