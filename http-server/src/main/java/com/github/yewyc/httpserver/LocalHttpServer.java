c32bea917901c274dbfc023a1ad70490331d2349
Diego Lovison <diegolovison@gmail.com>
2026-01-27 14:10:17 -0300

    Change the executor to newVirtualThreadPerTaskExecutor

--- http-server/src/main/java/com/github/yewyc/httpserver/LocalHttpServer.java
+++ http-server/src/main/java/com/github/yewyc/httpserver/LocalHttpServer.java
@@ -6,13 +6,14 @@ import com.sun.net.httpserver.HttpServer;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.util.concurrent.Executors;
 
 public class LocalHttpServer {
 
     public static void main(String[] args) throws IOException {
         HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
         server.createContext("/hello", new HelloHandler());
-        server.setExecutor(null); // creates a default executor
+        server.setExecutor(Executors.newVirtualThreadPerTaskExecutor()); // The number of threads created by the Executor is unbounded
         server.start();
     }
 
