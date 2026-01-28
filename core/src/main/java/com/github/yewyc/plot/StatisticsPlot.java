cb3111af99407c02ece009ce761082ea04095bd3
Diego Lovison <diegolovison@gmail.com>
2026-01-28 15:26:54 -0300

    throughput chart

--- core/src/main/java/com/github/yewyc/plot/StatisticsPlot.java
+++ core/src/main/java/com/github/yewyc/plot/StatisticsPlot.java
@@ -2,48 +2,78 @@ package com.github.yewyc.plot;
 
 import com.github.yewyc.stats.Statistics;
 import tech.tablesaw.plotly.Plot;
+import tech.tablesaw.plotly.components.Axis;
+import tech.tablesaw.plotly.components.Config;
 import tech.tablesaw.plotly.components.Figure;
+import tech.tablesaw.plotly.components.Grid;
 import tech.tablesaw.plotly.components.Layout;
 import tech.tablesaw.plotly.traces.ScatterTrace;
 import tech.tablesaw.plotly.traces.Trace;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
 public class StatisticsPlot {
 
     public static void plot(List<Statistics> tasks) {
-        List<Trace> traces = new ArrayList<>();
+        List<Trace> allTraces = new ArrayList<>();
 
         double globalXIndex = 1;
 
         for (int i = 0; i < tasks.size(); i++) {
             Statistics stats = tasks.get(i);
             double[][] xy = stats.getXY();
-            double[] yValuesRaw = xy[1]; // Get Y values
+            double[] latencyRaw = xy[1];
+            double[] throughputRaw = xy[2];
 
-            double[] xValues = new double[yValuesRaw.length];
-            double[] yValues = new double[yValuesRaw.length];
+            double[] xValues = new double[latencyRaw.length];
+            double[] latencyValues = new double[latencyRaw.length];
+            double[] throughputValues = new double[latencyRaw.length];
 
-            for (int j = 0; j < yValuesRaw.length; j++) {
+            for (int j = 0; j < latencyRaw.length; j++) {
                 xValues[j] = globalXIndex;
-                yValues[j] = yValuesRaw[j];
+                latencyValues[j] = latencyRaw[j];
+                throughputValues[j] = throughputRaw[j];
                 globalXIndex++;
             }
 
-            ScatterTrace trace = ScatterTrace.builder(xValues, yValues)
+            allTraces.add(ScatterTrace.builder(xValues, latencyValues)
                     .mode(ScatterTrace.Mode.LINE)
-                    .name(stats.getName())
-                    .build();
+                    .name(stats.getName() + " (Lat)")
+                    .build());
 
-            traces.add(trace);
+            allTraces.add(ScatterTrace.builder(xValues, throughputValues)
+                    .mode(ScatterTrace.Mode.LINE)
+                    .xAxis("x2")
+                    .yAxis("y2")
+                    .name(stats.getName() + " (Thr)")
+                    .build());
         }
 
-        Layout layout = Layout.builder().title("Response Time Mean (ms)").build();
+        Grid grid = Grid.builder()
+                .rows(2)
+                .columns(1)
+                .pattern(Grid.Pattern.INDEPENDENT)
+                .build();
 
-        Figure figure = new Figure(layout, traces.toArray(new Trace[0]));
+        Layout layout = Layout.builder()
+                .title("Perf Report - " + new Date())
+                .grid(grid)
+                .autosize(true)
+                .height(800)
+                .yAxis(Axis.builder().title("Latency (ms)").build())
+                .yAxis2(Axis.builder().title("Throughput (ops/sec)").build())
+                .build();
+
+        Config config = Config.builder()
+                .responsive(true)
+                .build();
+
+        Figure figure = new Figure(layout, config, allTraces.toArray(new Trace[0]));
         Plot.show(figure, new File("/tmp/report-" + UUID.randomUUID() + ".html"));
     }
 }
+
