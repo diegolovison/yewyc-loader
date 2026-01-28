d5bbe534ec845f19734fbc8041d7a3ebf4a60feb
Diego Lovison <diegolovison@gmail.com>
2026-01-28 14:01:19 -0300

    Plot response time mean as a sequence for easy comparison

--- core/src/main/java/com/github/yewyc/plot/StatisticsPlot.java
+++ core/src/main/java/com/github/yewyc/plot/StatisticsPlot.java
@@ -3,7 +3,6 @@ package com.github.yewyc.plot;
 import com.github.yewyc.stats.Statistics;
 import tech.tablesaw.plotly.Plot;
 import tech.tablesaw.plotly.components.Figure;
-import tech.tablesaw.plotly.components.Grid;
 import tech.tablesaw.plotly.components.Layout;
 import tech.tablesaw.plotly.traces.ScatterTrace;
 import tech.tablesaw.plotly.traces.Trace;
@@ -13,37 +12,38 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-import static tech.tablesaw.plotly.traces.ScatterTrace.Fill.TO_ZERO_Y;
-
 public class StatisticsPlot {
 
     public static void plot(List<Statistics> tasks) {
         List<Trace> traces = new ArrayList<>();
-        // `i` is 1 because of https://github.com/jtablesaw/tablesaw/issues/1284
-        int i = 1;
-        for (Statistics stats : tasks) {
-            PlotData plotData = StatisticsPlot.plot(stats, i);
-            traces.add(plotData.trace);
-            i += 1;
-        }
-        if (traces.size() > 0) {
-            Grid grid = Grid.builder().columns(1).rows(traces.size()).pattern(Grid.Pattern.INDEPENDENT).build();
-            Layout layout = Layout.builder().width(1700).height(800).title("Response time mean(ms)").grid(grid).build();
-            Figure figure = new Figure(layout, traces.stream().toArray(Trace[]::new));
-            Plot.show(figure, new File("/tmp/report-" + UUID.randomUUID() + ".html"));
-        }
-    }
 
-    private static PlotData plot(Statistics stats, int chartIndex) {
-        double[][] xy = stats.getXY();
-        double[] xData = xy[0];
-        double[] yData = xy[1];
-        ScatterTrace trace = ScatterTrace.builder(xData, yData)
-                .mode(ScatterTrace.Mode.LINE)
-                .name(stats.getName())
-                .xAxis("x" + chartIndex).yAxis("y" + chartIndex)
-                .fill(TO_ZERO_Y)
-                .build();
-        return new PlotData(xData, yData, trace);
+        double globalXIndex = 1;
+
+        for (int i = 0; i < tasks.size(); i++) {
+            Statistics stats = tasks.get(i);
+            double[][] xy = stats.getXY();
+            double[] yValuesRaw = xy[1]; // Get Y values
+
+            double[] xValues = new double[yValuesRaw.length];
+            double[] yValues = new double[yValuesRaw.length];
+
+            for (int j = 0; j < yValuesRaw.length; j++) {
+                xValues[j] = globalXIndex;
+                yValues[j] = yValuesRaw[j];
+                globalXIndex++;
+            }
+
+            ScatterTrace trace = ScatterTrace.builder(xValues, yValues)
+                    .mode(ScatterTrace.Mode.LINE)
+                    .name(stats.getName())
+                    .build();
+
+            traces.add(trace);
+        }
+
+        Layout layout = Layout.builder().title("Response Time Mean (ms)").build();
+
+        Figure figure = new Figure(layout, traces.toArray(new Trace[0]));
+        Plot.show(figure, new File("/tmp/report-" + UUID.randomUUID() + ".html"));
     }
 }
