d17c52533e242873758d2a5355c083779e806a5e
Diego Lovison <diegolovison@gmail.com>
2026-02-05 13:43:46 -0300

    wrk3

---
+++ core/src/main/java/com/github/yewyc/wrk/Wrk3.java
@@ -0,0 +1,19 @@
+package com.github.yewyc.wrk;
+
+import com.github.yewyc.benchmark.Benchmark;
+
+// -XX:+FlightRecorder -XX:FlightRecorderOptions=stackdepth=128 -XX:StartFlightRecording=maxsize=1g,dumponexit=true,filename=data.jfr,settings=default
+public class Wrk3 extends WrkAbstract {
+
+    public static void main(String[] args) {
+        Wrk3 wrk2 = new Wrk3();
+        wrk2.exec(args);
+    }
+
+    @Override
+    protected void validate(Benchmark benchmark) {
+        if (benchmark.getRecord().rate() <= 0) {
+            throw new IllegalStateException("Rate should be greater than 0");
+        }
+    }
+}
